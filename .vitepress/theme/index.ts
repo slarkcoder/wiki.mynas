9cb211fd07a24d36f87311f3e5c733b7c250e67b
Slark <slarker@qq.com>
2024-09-04 23:56:26 +0800

    update

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,6 +1,12 @@
 import Theme from "vitepress/theme"
+import googleAnalytics from 'vitepress-plugin-google-analytics'
 import './style/var.css'
 
 export default {
-	...Theme
+	...Theme,
+	enhanceApp: (ctx) => {
+		googleAnalytics({
+		  id: 'G-GZXN8CPFKZ', // Replace with your GoogleAnalytics ID, which should start with the 'G-'
+		})
+	  }
 }
