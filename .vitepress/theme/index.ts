f34136d3fdbf2e868ae487138b7cd03025fea612
Slark <slarker@qq.com>
2024-09-05 00:09:10 +0800

    update

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,12 +1,22 @@
 import Theme from "vitepress/theme"
-import googleAnalytics from 'vitepress-plugin-google-analytics'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import './style/var.css'
 
 export default {
-	...Theme,
-	enhanceApp: (ctx) => {
-		googleAnalytics({
-		  id: 'G-GZXN8CPFKZ', // Replace with your GoogleAnalytics ID, which should start with the 'G-'
-		})
-	  }
+	...Theme
 }
+
+const firebaseConfig = {
+	apiKey: "AIzaSyDcKx1dBRVlvaoR62jKeR_t22EnPgZsy3k",
+	authDomain: "nas-wiki.firebaseapp.com",
+	projectId: "nas-wiki",
+	storageBucket: "nas-wiki.appspot.com",
+	messagingSenderId: "438774902687",
+	appId: "1:438774902687:web:f160b922e7b50d5ef197aa",
+	measurementId: "G-YHZ0PVFXQ3"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
